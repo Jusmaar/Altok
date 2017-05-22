9fa080aa37b2f3ef44714bcb0d7ca5a79c789ed2
Jusmar <dev03.easymaq@gmail.com>
2017-05-22 17:56:51 -0500

    Version_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+declare var jQuery:any;
+declare var $:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app works!';
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SliderHomeComponent } from './componentes/slider-home/slider-home.component';
+import { PrestamosComponent } from './componentes/prestamos/prestamos.component';
+import { PasosComponent } from './componentes/pasos/pasos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderHomeComponent,
+    PrestamosComponent,
+    PasosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
